7b1fef2be781d9b9db03a717e8d9a3f0876ed247
Grossgl <130256033+Grossgl@users.noreply.github.com>
2024-12-08 19:17:05 -0500

    Update views.py

--- views.py
+++ views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_new(request):
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
