c7a9ca31f162f5cb2e10ccfe7cb5346177c7b313
Grossgl <130256033+Grossgl@users.noreply.github.com>
2024-12-08 19:28:09 -0500

    Update views.py

--- views.py
+++ views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
@@ -16,6 +18,7 @@ def post_new(request):
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
